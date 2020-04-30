beaa0f692045085dec232dc4596cfbaa5420a3dc
alexander <alexanderkulnyow@yahoo.com>
2020-04-30 19:18:34 +0300

    init

--- staff_posttype.php
+++ staff_posttype.php
@@ -25,15 +25,27 @@
 |--------------------------------------------------------------------------
 */
 
-if( !defined( 'staff__posttype_BASE_FILE' ) )		define( 'staff__posttype_BASE_FILE', __FILE__ );
-if( !defined( 'staff__posttype_BASE_DIR' ) ) 		define( 'staff__posttype_BASE_DIR', dirname( staff__posttype_BASE_FILE ) );
-if( !defined( 'staff__posttype_PLUGIN_URL' ) ) 	define( 'staff__posttype_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+if ( ! defined( 'staff__posttype_BASE_FILE' ) ) {
+	define( 'staff__posttype_BASE_FILE', __FILE__ );
+}
+if ( ! defined( 'staff__posttype_BASE_DIR' ) ) {
+	define( 'staff__posttype_BASE_DIR', dirname( staff__posttype_BASE_FILE ) );
+}
+if ( ! defined( 'staff__posttype_PLUGIN_URL' ) ) {
+	define( 'staff__posttype_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
 
 
+/*
+|--------------------------------------------------------------------------
+| Styles
+|--------------------------------------------------------------------------
+*/
 function staff__posttype_scripts() {
 	wp_enqueue_style( 'penus', staff__posttype_PLUGIN_URL . 'styles/style.css', array(), '1.0.0' );
 //	wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
 }
+
 add_action( 'wp_enqueue_scripts', 'staff__posttype_scripts' );
 /*
 |--------------------------------------------------------------------------
@@ -43,7 +55,7 @@ add_action( 'wp_enqueue_scripts', 'staff__posttype_scripts' );
 add_filter( 'init', 'post_type_staff_register' );
 add_filter( 'init', 'taxonomy_tax_staff_register' );
 add_filter( 'admin_init', 'add_custom_taxonomy' );
-add_filter( 'template_include', 'staff__posttype_template_chooser');
+add_filter( 'template_include', 'staff__posttype_template_chooser' );
 /*
 |--------------------------------------------------------------------------
 | DEFINE THE CUSTOM TAXONOMY
@@ -113,6 +125,7 @@ function add_custom_taxonomy() {
 	wp_insert_category( $lab_assist );
 
 }
+
 /*
 |--------------------------------------------------------------------------
 | DEFINE THE CUSTOM POST TYPE
@@ -168,21 +181,19 @@ function post_type_staff_register() {
  * @since       1.0
  */
 
-function staff__posttype_template_chooser($template) {
+function staff__posttype_template_chooser( $template ) {
 	// Post ID
 	$post_id = get_the_ID();
 	// For all other CPT
-	if( get_post_type( $post_id ) != 'staff' ) {
+	if ( get_post_type( $post_id ) != 'staff' ) {
 		return $template;
 	} else {
 		if ( is_archive() ) {
 //		staff__posttype_PLUGIN_URL . 'includes/templates/archive-staff.php';
-			return staff__posttype_get_template_hierarchy('archive-staff');
-		}
-		elseif ( is_single() ) {
-			return staff__posttype_get_template_hierarchy('single-staff');
-		}
-		else {
+			return staff__posttype_get_template_hierarchy( 'archive-staff' );
+		} elseif ( is_single() ) {
+			return staff__posttype_get_template_hierarchy( 'single-staff' );
+		} else {
 			return null;
 		}
 	}
@@ -197,16 +208,15 @@ function staff__posttype_template_chooser($template) {
 function staff__posttype_get_template_hierarchy( $template ) {
 
 	// Get the template slug
-	$template_slug = rtrim($template, '.php');
+	$template_slug = rtrim( $template, '.php' );
 	$template      = $template_slug . '.php';
 
 	// Check if a custom template exists in the theme folder, if not, load the plugin template file
-	if ( $theme_file = locate_template(array('plugin_template/'.$template)) ) {
+	if ( $theme_file = locate_template( array( 'plugin_template/' . $template ) ) ) {
 		$file = $theme_file;
-	}
-	else {
+	} else {
 		$file = staff__posttype_BASE_DIR . '/includes/templates/' . $template;
 	}
 
-	return apply_filters( 'staff__posttype_repl_template_'.$template, $file);
+	return apply_filters( 'staff__posttype_repl_template_' . $template, $file );
 }
